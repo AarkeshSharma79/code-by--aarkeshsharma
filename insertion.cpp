2ca8c250d8af4c2846a6eaf1edd8f7109503dcb5
AarkeshSharma79 <aarkeshsharma17@gmail.com>
2025-09-04 21:19:51 +0530

    it is the code of inserting any element in array

---
+++ insertion.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,a[100],n,pos,m;
+cout<<"enter the size of array";
+cin>>n;
+for(i=0;i<n;i++)
+{
+    cin>>a[i];
+}
+cout<<"enter the position of element where you want insert element";
+cin>>pos;
+cout<<"enter the number you want insert in array";
+cin>>m;
+for(i=n;i>=pos;i--)
+{
+ a[i]=a[i-1];
+}
+a[pos]=m;
+n++;
+
+for(i=0;i<n;i++)
+{
+    cout<<a[i]<<" ";
+}
+return 0;
+}
